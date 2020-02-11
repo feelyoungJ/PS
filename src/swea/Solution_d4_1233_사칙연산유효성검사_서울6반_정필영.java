3533ad9daa99d499fb8c2e77d5f8ebae198a9ef0
feelyoungJ <jpy5782@naver.com>
2020-02-11 20:42:23 +0900

    0211

--- src/swea/Solution_d4_1233_사칙연산유효성검사_서울6반_정필영.java
+++ src/swea/Solution_d4_1233_사칙연산유효성검사_서울6반_정필영.java
@@ -16,56 +16,40 @@ public class Solution_d4_1233_사칙연산유효성검사_서울6반_정필영 {
 		for (int tc=1; tc<11; tc++) {
 			N = Integer.parseInt(br.readLine());
 			
-			tree = new ArrayList[N+1];
+			tree = new ArrayList[N];
 			result = 0;
 			
-			for (int i=1; i<N+1; i++) {
+			for (int i=0; i<N; i++) {
 				tree[i] = new ArrayList<>();
 				st = new StringTokenizer(br.readLine());
 				while (st.hasMoreTokens()) {
 					tree[i].add(st.nextToken());
 				}
 			}
-//			System.out.println(Arrays.toString(tree));
-			inorder(1);
-			System.out.println();
-//			System.out.println(Arrays.toString(tree));
-//			System.out.println(tree.length);
-//			System.out.println("#" + tc + " " + result);
+			
+			boolean flag = true;
+			for (int i=0; i<N; i++) {
+				//String s = tree[i].get(1);
+					
+				if (check(tree[i].get(1))) {
+					if (tree[i].size()!=4 || check(tree[i].get(2)) || check(tree[i].get(3))) {
+						flag = false;
+						break;
+					}
+				} else {
+					if (tree[i].size()!=2) {
+						flag = false;
+						break;
+					}
+				}
+			}
+			
+			if (flag) System.out.println("#" + tc + " " + 1);
+			else System.out.println("#" + tc + " " + 0);
 		}
 	}
-	public static boolean inorder(int i) {
-//		if (N>=i) {
-			String s = tree[i].get(1);
-			if (tree[i].size()==4) {
-				inorder(i*2);
-				inorder(i*2+1);
-			} else if (tree[i].size()==2 && !s.equals("+") && !s.equals("-") && !s.equals("*") && !s.equals("/")){
-				return true;
-			} 
-			return false;
-			
-			
-//			System.out.print(tree[i].get(1));
-			
-			
-			
-			
-//		}
-//		return true;
-		
-//		String s = tree[i].get(1);
-//		
-//		if (s.equals("+")) {
-//			return inorder(Integer.parseInt(tree[i].get(2))) + inorder(Integer.parseInt(tree[i].get(3)));
-//		} else if (s.equals("-")) {
-//			return inorder(Integer.parseInt(tree[i].get(2))) - inorder(Integer.parseInt(tree[i].get(3)));
-//		} else if (s.equals("*")) {
-//			return inorder(Integer.parseInt(tree[i].get(2))) * inorder(Integer.parseInt(tree[i].get(3)));
-//		} else if (s.equals("/")) {
-//			return inorder(Integer.parseInt(tree[i].get(2))) / inorder(Integer.parseInt(tree[i].get(3)));
-//		} else {
-//			return Double.parseDouble(tree[i].get(1));
-//		}
+	
+	public static boolean check(String s) {
+		return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/");
 	}
 }
